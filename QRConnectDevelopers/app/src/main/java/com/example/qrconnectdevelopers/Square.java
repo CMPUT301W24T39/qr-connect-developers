fbaeea6d161ee30364d6f1d8ab8246678ae6d48b
kevinfrito <103242962+kevinfrito@users.noreply.github.com>
2024-02-01 22:20:41 -0700

    Create Square.java

---
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Square.java
@@ -0,0 +1,19 @@
+package com.example.qrconnectdevelopers;
+
+public class Square extends Shape{
+
+
+    public Square(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
