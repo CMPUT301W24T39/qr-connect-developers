d1df00af5199a04874dfe69873e8273824a27c4a
Jill Ference <ferencejill@gmail.com>
2024-02-02 11:36:40 -0700

    default

--- QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
@@ -4,9 +4,6 @@ public abstract class Shape {
     int x;
     int y;
     
-    String color = "blue";
-    String color = "red";
-
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
