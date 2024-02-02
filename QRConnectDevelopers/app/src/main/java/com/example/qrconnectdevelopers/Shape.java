d539b67ea96786e661818cbc74b35bd682cafc24
Luwei-Lin <luwei2@ualberta.ca>
2024-02-01 19:52:29 -0700

    Solved the conflict.

--- QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
@@ -3,14 +3,9 @@ package com.example.qrconnectdevelopers;
 public abstract class Shape {
     int x;
     int y;
-
-<<<<<<< HEAD
-    protected String color = "purple"; // Default color is blue
-
+    
     String color = "dark";
-=======
-    String color = "pink"; 
->>>>>>> 4435e2d (Updated color from my branch.)
+
 
     public Shape(int x, int y) {
         this.x = x;
