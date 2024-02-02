69bbd1ed35be5330b1129ca7e80fc59854333291
Jill Ference <ferencejill@gmail.com>
2024-02-02 00:17:03 -0700

    colour red in Shape

--- QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
@@ -3,9 +3,7 @@ package com.example.qrconnectdevelopers;
 public abstract class Shape {
     int x;
     int y;
-
-    String color = "blue";
-
+    
     String color = "red";
 
     public Shape(int x, int y) {
