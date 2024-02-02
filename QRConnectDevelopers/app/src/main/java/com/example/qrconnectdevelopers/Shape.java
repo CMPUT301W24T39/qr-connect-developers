94034f9587f5bc42e6c67f11bef11c4798b4a016
Luwei-Lin <luwei2@ualberta.ca>
2024-02-01 19:02:55 -0700

    Created an abstract class Shape.
    An abstract class Shape.with x, y integer fields and two abstract methods.

---
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
@@ -0,0 +1,31 @@
+package com.example.qrconnectdevelopers;
+
+public abstract class Shape {
+    int x;
+    int y;
+
+    public Shape(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
