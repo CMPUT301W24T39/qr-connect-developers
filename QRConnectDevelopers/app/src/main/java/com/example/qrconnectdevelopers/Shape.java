5ed4a0a320fd56253933a47fcb4acf60834ec1df
Billyisher <zhuqiao080@gmail.com>
2024-02-01 19:36:22 -0700

    Update Shape.java

--- QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Shape.java
@@ -4,7 +4,7 @@ public abstract class Shape {
     int x;
     int y;
 
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
