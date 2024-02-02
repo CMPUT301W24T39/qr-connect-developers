d6968f130963e423952d081b922889ee7c9f3092
Luwei-Lin <luwei2@ualberta.ca>
2024-02-01 19:17:52 -0700

    Created Class Triangle which extends from Shape.

---
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Triangle.java
@@ -0,0 +1,17 @@
+package com.example.qrconnectdevelopers;
+
+public class Triangle extends Shape{
+
+    public Triangle(){
+        super(1, 2);
+    }
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
