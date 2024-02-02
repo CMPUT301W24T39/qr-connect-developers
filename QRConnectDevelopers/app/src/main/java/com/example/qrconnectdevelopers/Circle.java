7835f2f99e276abcd49ab5cc8105070632cbe20e
Billyisher <zhuqiao080@gmail.com>
2024-02-01 19:14:32 -0700

    Created Circle class

---
+++ QRConnectDevelopers/app/src/main/java/com/example/qrconnectdevelopers/Circle.java
@@ -0,0 +1,19 @@
+package com.example.qrconnectdevelopers;
+
+public class Circle extends Shape{
+
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public double perimeter() {
+        return 0;
+    }
+}
