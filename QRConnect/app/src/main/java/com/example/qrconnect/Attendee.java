406a31745264bf4f8f1297ff3a4d8c55c671ef2d
Luwei-Lin <luwei2@ualberta.ca>
2024-03-02 21:49:39 -0700

    Added some main classes
    Added main roles

---
+++ QRConnect/app/src/main/java/com/example/qrconnect/Attendee.java
@@ -0,0 +1,17 @@
+package com.example.qrconnect;
+
+public class Attendee extends User{
+    public Attendee(String userID, String name, String contactInformation) {
+        super(userID, name, contactInformation);
+    }
+
+    @Override
+    public void uploadProfilePicture(String imagePath) {
+
+    }
+
+    @Override
+    public void removeProfilePicture() {
+
+    }
+}
