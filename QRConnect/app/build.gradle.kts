39e9f397a0dba49757ba08e2edd22a8c385a25bc
Billyisher <zhuqiao080@gmail.com>
2024-03-02 22:36:19 -0700

    Updated generating QR code page and selecting existing QR code page

--- QRConnect/app/build.gradle.kts
+++ QRConnect/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,5 +39,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-firestore")
+    implementation ("com.journeyapps:zxing-android-embedded:4.3.0")
 }
--- QRConnect/app/src/main/java/com/example/qrconnect/MainActivity.java
+++ QRConnect/app/src/main/java/com/example/qrconnect/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.qrconnect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton addButton;
+
+    static ArrayList<Event> events = new ArrayList<Event>();
+
+    static boolean isAddButtonClicked = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addButton = findViewById(R.id.button_add_event);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, QRCodeGeneratesPage.class));
+                isAddButtonClicked = true;
+                Event newEvent = new Event();
+                events.add(newEvent);
+            }
+        });
+
+
     }
+
+
 }
