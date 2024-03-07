6a60ff0b85aa880ad782deb7a5cfd61a5391db9e
Will Chorkawy <wchorkaw@ualberta.ca>
2024-03-07 01:53:52 -0700

    user input button works, doesn't save

--- QRConnect/app/src/main/java/com/example/qrconnect/UserProfilePage.java
+++ QRConnect/app/src/main/java/com/example/qrconnect/UserProfilePage.java
@@ -1,46 +1,50 @@
 package com.example.qrconnect;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Objects;
+
 public class UserProfilePage extends AppCompatActivity {
 
     private Button saveButton;
-    private EditText firstNameEditText;
-    private EditText lastNameEditText;
-    private EditText pronounsEditText;
-    private EditText emailEditText;
-    private EditText phoneEditText;
+    private TextInputEditText firstNameEditText;
+    private TextInputEditText lastNameEditText;
+    private TextInputEditText pronounsEditText;
+    private TextInputEditText emailEditText;
+    private TextInputEditText phoneEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile_page);
 
-//        addCityEditText = findViewById(R.id.city_name_edit);
+        saveButton = findViewById(R.id.save_button);
+
         firstNameEditText = findViewById(R.id.first_name_edit);
         lastNameEditText = findViewById(R.id.last_name_edit);
         pronounsEditText = findViewById(R.id.pronouns_edit);
         emailEditText = findViewById(R.id.email_edit);
-        phoneEditText = findViewById(R.id.phone_edit);
+        phoneEditText =  findViewById(R.id.phone_edit);
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firstNameEditText.getText().clear();
-                lastNameEditText.getText().clear();
-                pronounsEditText.getText().clear();
-                emailEditText.getText().clear();
-                phoneEditText.getText().clear();
+                String firstName = firstNameEditText.getText().toString();
+                String lastName = lastNameEditText.getText().toString();
+                String pronouns = pronounsEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String phone = phoneEditText.getText().toString();
 
-                final String firstName = firstNameEditText.getText().toString();
-                final String lastName = lastNameEditText.getText().toString();
-                final String pronouns = pronounsEditText.getText().toString();
-                final String email = emailEditText.getText().toString();
-                final String phone = phoneEditText.getText().toString();
+                /*
+                TODO: connect this to firebase
+                 */
             }
         });
 
