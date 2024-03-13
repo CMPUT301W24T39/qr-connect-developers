55795849954987f9fd341290dac7978d94545bf2
Tanmay Lad <tlad@ualberta.ca>
2024-03-13 17:03:55 -0600

    Revert "Set up Firebase Anonymous Auth" Pushed on wrong banch. No modifications to previous files. This reverts commit 721963d91938c927d47f71291b9485defebd9b30.

--- QRConnect/app/src/main/java/com/example/qrconnect/SplashActivity.java
+++ QRConnect/app/src/main/java/com/example/qrconnect/SplashActivity.java
@@ -5,14 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 /**
  * The SplashActivity class represents the initial screen displayed when the application is launched.
@@ -21,8 +13,6 @@ import com.google.firebase.auth.FirebaseUser;
  * It extends AppCompatActivity.
  */
 public class SplashActivity extends AppCompatActivity {
-
-    private static final String TAG = "SplashActivity";
 
     /**
      * Called when the activity is first created. Responsible for initializing the splash screen.
@@ -39,42 +29,13 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                checkSignIn();
+                // Create an intent to transition to the UserStartScreen activity
+                Intent intent = new Intent(SplashActivity.this, UserStartScreen.class);
+                // Start the UserStartScreen activity
+                startActivity(intent);
+                // Finish the current SplashActivity to prevent going back to it using the back button
+                finish();
             }
         }, 3000); // 3000 milliseconds (3 seconds) delay
     }
-    private void checkSignIn() {
-        // Source: https://firebase.google.com/docs/auth/android/anonymous-auth#java
-
-        FirebaseAuth.getInstance().signInAnonymously()
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInAnonymously:success");
-                            // User signed in anonymously
-                            checkUserType();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SplashActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-    private void checkUserType() {
-        // Check if the user is already signed in anonymously
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null && currentUser.isAnonymous()) {
-            // Returning user (already signed in anonymously)
-            startActivity(new Intent(SplashActivity.this, ReturnUserStartScreen.class));
-        } else {
-            // New user not signed in anonymously
-            startActivity(new Intent(SplashActivity.this, UserStartScreen.class));
-        }
-
-        finish();
-    }
-
-
 }
