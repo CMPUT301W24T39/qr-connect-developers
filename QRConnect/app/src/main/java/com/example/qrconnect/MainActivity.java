c4520a04bf898ae96c0a14c2e1f0b7520b3414e1
Tanmay Lad <tlad@ualberta.ca>
2024-04-08 01:12:05 -0600

    add signup list in main activity

--- QRConnect/app/src/main/java/com/example/qrconnect/MainActivity.java
+++ QRConnect/app/src/main/java/com/example/qrconnect/MainActivity.java
@@ -461,7 +461,8 @@ public class MainActivity extends AppCompatActivity implements DeleteEventFragme
      */
     private void getUserEvents() {
         for (Event event : eventDataList) {
-            if (event.getHostId() != null && event.getHostId().equals(userId) || event.getAttendeeListIdToName().containsKey(userId)) {
+            if (event.getHostId() != null && event.getHostId().equals(userId) || event.getAttendeeListIdToName().containsKey(userId)
+                || event.getSignupUserIdToName().containsKey(userId)) {
                 userEventDataList.add(event);
             }
         }
